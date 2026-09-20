8e599a771b11ef1a9fcee083c66dca65c4b95e0a
Ataberk Selekoglu <59377994+aselekoglu@users.noreply.github.com>
2026-09-20 19:47:56 -0400

    feat: land login on canonical listings

--- ui/src/views/login/Login.jsx
+++ ui/src/views/login/Login.jsx
@@ -38,7 +38,7 @@ export default function Login() {
 
     Toast.success('Login successful!');
     await actions.user.getCurrentUser();
-    navigate('/jobs');
+    navigate('/listings');
   };
 
   return (
